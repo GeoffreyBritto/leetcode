9d2434fb281863c99f1c733177df43adfec16658
GeoffreyBritto <159030646+GeoffreyBritto@users.noreply.github.com>
2024-03-23 09:27:38 +0530

    Time: 2 ms (88.69%), Space: 48.5 MB (22.58%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return;
+        
+        // Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Split the list into two halves
+        ListNode secondHalf = reverseList(slow.next);
+        slow.next = null; // break the link
+        
+        // Interleave nodes from the first and second halves
+        ListNode p1 = head;
+        ListNode p2 = secondHalf;
+        while (p1 != null && p2 != null) {
+            ListNode temp1 = p1.next;
+            ListNode temp2 = p2.next;
+            p1.next = p2;
+            p2.next = temp1;
+            p1 = temp1;
+            p2 = temp2;
+        }
+    }
+    
+    // Helper function to reverse a linked list
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
