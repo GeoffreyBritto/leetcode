f22393e8b01f99592ffcb464879dd5ae27289b42
GeoffreyBritto <159030646+GeoffreyBritto@users.noreply.github.com>
2024-07-25 18:16:55 +0530

    Time: 399 ms (85.27%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,6 @@
 /* Write your T-SQL query statement below */
 SELECT w1.id 
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(day, w2.recordDate, w1.recordDate) = 1 
-AND w1.temperature > w2.temperature;  
+FROM Weather w1
+JOIN Weather w2
+ON w1.recordDate = DATEADD(day, 1, w2.recordDate)
+WHERE w1.temperature > w2.temperature
